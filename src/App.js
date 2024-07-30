287ced33f91e3cc72fa4c1ac1b89bcb6c35e48f0
Abdillah AG <abdillahag98@gmail.com>
2024-07-30 10:14:33 +0700

    add a-frame component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import 'aframe';
+import 'aframe-ar';
 import React from 'react';
 
 function App() {
   return (
-    <div style={{'margin': '0px', 'overflow': 'hidden'}}>
+    <div style={{ margin: '0px', overflow: 'hidden' }}>
       <a-scene embedded arjs='sourceType: webcam;'>
         <a-box position='0 0.5 0' color="#EF2D5E"></a-box>
         <a-marker-camera preset='hiro'></a-marker-camera>
