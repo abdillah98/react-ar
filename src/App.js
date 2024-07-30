693a63a35e1143712a612f2bb1afa4f6c3ad58a9
Abdillah AG <abdillahag98@gmail.com>
2024-07-30 10:43:47 +0700

    modify a-frame component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div style={{ margin: '0px', overflow: 'hidden' }}>
-       <a-scene arjs='sourceType: webcam; debugUIEnabled: false'>
+       <a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false'>
         <a-anchor hit-testing-enabled="true">
           <a-box position='0 0.5 0' color="#EF2D5E"></a-box>
         </a-anchor>
