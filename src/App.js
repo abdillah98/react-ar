21d4d9f9b841490b350fd4f86f72f719659fe3ea
Abdillah AG <abdillahag98@gmail.com>
2024-07-30 10:28:02 +0700

    modify a-frame component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import 'aframe';
-import 'aframe-ar';
 import React from 'react';
 
 function App() {
   return (
     <div style={{ margin: '0px', overflow: 'hidden' }}>
-      <a-scene embedded arjs='sourceType: webcam;'>
+      {/* <a-scene embedded arjs='sourceType: webcam;'>
         <a-box position='0 0.5 0' color="#EF2D5E"></a-box>
         <a-marker-camera preset='hiro'></a-marker-camera>
-      </a-scene>
+      </a-scene> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+setTimeout(
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ), 1500
 );
 
 // If you want to start measuring performance in your app, pass a function
