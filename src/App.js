1c5122017f4fd0c1db3c97dfb0d97ac934c56b78
Abdillah AG <abdillahag98@gmail.com>
2024-07-30 09:53:19 +0700

    add a-frame component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React from 'react';
 function App() {
   return (
     <div>
-      {/* <a-scene embedded arjs='sourceType: webcam;'>
+      <a-scene embedded arjs='sourceType: webcam;'>
         <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
         <a-marker-camera preset='hiro'></a-marker-camera>
-      </a-scene> */}
+      </a-scene>
     </div>
   );
 }
